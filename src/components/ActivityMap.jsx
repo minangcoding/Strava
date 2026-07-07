9f5e1a89010faceedd521431b0586d1598ead52d
Refido Arjunal Akmal <refidoarjunalakmal@gmail.com>
2026-07-07 12:49:11 +0700

    Memperbaiki bug ikon marker peta hilang di production

--- src/components/ActivityMap.jsx
+++ src/components/ActivityMap.jsx
@@ -1,6 +1,19 @@
 // src/components/ActivityMap.jsx
 import { MapContainer, TileLayer, Polyline, Marker, useMap } from 'react-leaflet'
 import { useEffect } from 'react'
+import L from 'leaflet'
+
+// Fix untuk masalah ikon marker Leaflet yang hilang saat di-deploy (Vite production)
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
+import markerIcon from 'leaflet/dist/images/marker-icon.png'
+import markerShadow from 'leaflet/dist/images/marker-shadow.png'
+
+delete L.Icon.Default.prototype._getIconUrl
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIcon,
+  iconRetinaUrl: markerIcon2x,
+  shadowUrl: markerShadow,
+})
 
 // Komponen mini untuk otomatis mengarahkan kamera peta ke tengah rute
 function RecenterMap({ positions }) {
